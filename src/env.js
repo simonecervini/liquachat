e1ea30168c50f0adfe02b4ae3e11e19d895ee7c4
Simone Cervini <sc.cervini@gmail.com>
2025-06-14 18:52:58 +0200

    Add GitHub login

--- src/env.js
+++ src/env.js
@@ -16,6 +16,8 @@ export const env = createEnv({
     ZERO_PUSH_URL: z.string().url(),
     BETTER_AUTH_URL: z.string().url(),
     BETTER_AUTH_SECRET: z.string(), // openssl rand -base64 32
+    GITHUB_CLIENT_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
   },
 
   /**
@@ -40,6 +42,8 @@ export const env = createEnv({
     ZERO_PUSH_URL: process.env.ZERO_PUSH_URL,
     BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     // client
     NEXT_PUBLIC_ZERO_SERVER_URL: process.env.NEXT_PUBLIC_ZERO_SERVER_URL,
   },
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -47,6 +47,12 @@ export const auth = betterAuth({
       },
     },
   },
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
+  },
   advanced: {
     database: {
       generateId: () => {
